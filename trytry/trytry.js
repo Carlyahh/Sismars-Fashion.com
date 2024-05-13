b71b041762ec957802d32bd584dd96669c41e2b1
Carlyahh <orelcarlgabriel@gmail.com>
2024-05-13 17:17:11 +0800

    Add files via upload

---
+++ trytry/trytry.js
@@ -0,0 +1,14 @@
+var companySlogan = "Hi there Shoppers! Welcome to Sismars' Fashion, Where Style Meets Comfort.";
+function displayCompanySlogan() {
+    window.alert(companySlogan);
+}
+displayCompanySlogan();
+
+var productType = "bags";
+var promotionType = "sale";
+var discountPercentage = 50;
+var marketingMessage = "Hurry! " + promotionType + " on " + productType + ". Get " + discountPercentage + "% off on selected items!";
+window.alert(marketingMessage);
+
+var feedback = prompt("Please share your feedback about our products and services:");
+window.alert("Thank you for your feedback: " + feedback);
